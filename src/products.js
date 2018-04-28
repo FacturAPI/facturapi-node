ac14fb0268666bf20cc9afca1aa104c27f543c56
Kuriel <ikuriel@gmail.com>
2018-04-27 21:04:40 -0500

    Formato de JS a Standard Ajustes de documentación
    Signed-off-by: Kuriel <ikuriel@gmail.com>

--- src/products.js
+++ src/products.js
@@ -1,76 +1,76 @@
-'use strict';
+'use strict'
 
 class Products {
-    /**
-     * @param {Wrapper} wrapper
-     */
-    constructor(wrapper) {
-        this.wrapper = wrapper;
-    }
+  /**
+   * @param {Wrapper} wrapper
+   */
+  constructor (wrapper) {
+    this.wrapper = wrapper
+  }
 
-    /**
-     * Creates a new product in your organization
-     * @param {Object} data
-     * @returns {Promise}
-     */
-    create(data) {
-        return this.wrapper.createProduct(data);
-    }
+  /**
+   * Creates a new product in your organization
+   * @param {Object} data
+   * @returns {Promise}
+   */
+  create (data) {
+    return this.wrapper.createProduct(data)
+  }
 
-    /**
-     * Gets a paginated list of products that belong to your organization
-     * @param {[Object]} params - Search parameters
-     * @returns {Promise}
-     */
-    list(params) {
-        return this.wrapper.listProducts(params);
-    }
+  /**
+   * Gets a paginated list of products that belong to your organization
+   * @param {[Object]} params - Search parameters
+   * @returns {Promise}
+   */
+  list (params) {
+    return this.wrapper.listProducts(params)
+  }
 
-    /**
-     * Gets a single product object
-     * @param {string} id
-     * @returns {Promise}
-     */
-    retrieve(id) {
-        return this.wrapper.retrieveProduct(id);
-    }
+  /**
+   * Gets a single product object
+   * @param {string} id
+   * @returns {Promise}
+   */
+  retrieve (id) {
+    return this.wrapper.retrieveProduct(id)
+  }
 
-    /**
-     * Updates a product
-     * @param {string} id
-     * @param {Object} data
-     * @returns {Promise}
-     */
-    update(id, data) {
-        return this.wrapper.updateProduct(id, data);
-    }
+  /**
+   * Updates a product
+   * @param {string} id
+   * @param {Object} data
+   * @returns {Promise}
+   */
+  update (id, data) {
+    return this.wrapper.updateProduct(id, data)
+  }
 
-    /**
-     * Permanently removes a product from your organization.
-     * @param {string} id
-     * @returns {Promise}
-     */
-    del(id) {
-        return this.wrapper.removeProduct(id);
-    }
+  /**
+   * Permanently removes a product from your organization.
+   * @param {string} id
+   * @returns {Promise}
+   */
+  del (id) {
+    return this.wrapper.removeProduct(id)
+  }
 
-    /**
-     * Searches products by criteria "q"
-     * @param {string} q
-     * @returns {Promise}
-     */
-    keys(q) {
-        return this.wrapper.keys(q);
-    }
+  /**
+   * Searches product keys by criteria
+   * @param {string} criteria
+   * @returns {Promise}
+   */
+  keys (criteria) {
+    return this.wrapper.keys(criteria)
+  }
 
-    /**
-     * Searches products units by criteria "q"
-     * @param {string} q
-     * @returns {Promise}
-     */
-    units(q) {
-        return this.wrapper.units(q);
-    }
+  /**
+   * Searches products units by criteria
+   * @param {string} criteria
+   * @returns {Promise}
+   */
+  units (criteria) {
+    return this.wrapper.units(criteria)
+  }
 }
 
-module.exports = Products;
+module.exports = Products
