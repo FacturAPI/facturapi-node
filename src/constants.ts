f3c932116eddac9de861c00727cd33dabb959b20
javorosas <javier.rosasb@gmail.com>
2024-11-07 22:49:49 +0100

    fix unresolved process issue

--- src/constants.ts
+++ src/constants.ts
@@ -2,5 +2,7 @@ export const BASE_URL = 'https://www.facturapi.io/v2';
 export const BASE_URL_V1 = 'https://www.facturapi.io/v1';
 export const DEFAULT_API_VERSION = 'v2';
 
-export const isNode = Boolean(process?.versions?.node);
-export const isReactNative = navigator?.product === 'ReactNative';
+export const isNode =
+  typeof process !== 'undefined' && Boolean(process?.versions?.node);
+export const isReactNative =
+  typeof navigator !== 'undefined' && navigator?.product === 'ReactNative';
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,4 @@
-import { defineConfig, PluginOption } from 'vite';
-import { nodeResolve } from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
-import replace from '@rollup/plugin-replace';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
   build: {
@@ -11,21 +8,10 @@ export default defineConfig({
       fileName: (format) => `index.${format}.js`,
     },
     rollupOptions: {
-      external: ['node-fetch', 'form-data'],
+      external: ['form-data'],
       output: {
         exports: 'named', // Use named exports to avoid the warning
       },
-      plugins: [
-        nodeResolve({
-          browser: true,
-          preferBuiltins: false,
-        }) as PluginOption,
-        commonjs() as PluginOption,
-        replace({
-          'process.env.NODE_ENV': JSON.stringify('production'),
-          preventAssignment: true,
-        }) as PluginOption,
-      ],
     },
   },
 });
